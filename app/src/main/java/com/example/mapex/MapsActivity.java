a860ac7e64b5d47346520b10b50c008417c168bc
joococo <2000violet@naver.com>
2022-08-24 21:19:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mapex/MapsActivity.java
@@ -0,0 +1,234 @@
+package com.example.mapex;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+
+    //위치 정보 얻는 객체
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    //권한 체크 요청 코드 정의
+    public static  final int REQUEST_CODE_PERMISSIONS = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+
+        //GoogleAPIClient 인스턴스 생성
+        if(mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        ToggleButton b_mapMode = findViewById(R.id.b_mapmode);
+        b_mapMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                else
+                    mMap.setMapType((GoogleMap.MAP_TYPE_NORMAL));
+            }
+        });
+
+        ToggleButton b_showCurPos = findViewById(R.id.b_showCurPos);
+        b_showCurPos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //권한 체크
+                if(ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                                !=PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MapsActivity.this,
+                            new String[]{ Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION },
+                            REQUEST_CODE_PERMISSIONS);
+                    return;
+                }
+                mMap.setMyLocationEnabled(isChecked);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //마커 추가
+        LatLng ds = new LatLng(37.65132619696229, 127.01612821175677);
+        mMap.addMarker(new MarkerOptions().position(ds).title("Duksung University"));
+
+        //카메라 이동
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ds));
+
+        //카메라 줌인(2.0f ~ 21.0f)
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+
+        //마커 텍스트 클릭 이벤트트
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener(){
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:02-123-5678"));
+
+                startActivity(intent);
+            }
+        });
+
+        UiSettings ui = mMap.getUiSettings();
+
+        ui.setZoomControlsEnabled(true); //확대 축소
+
+        //false일 때 막아짐 (기본적으로 true, 설정되어있음)
+        //ui.setCompassEnabled(true); //나침판 (방향 움직여야 나옴) default
+        //ui.setMyLocationButtonEnabled(true); //default (현재위치보이기 활성화했을 때 맨위 오른쪽 표시)
+        //ui.setScrollGesturesEnabled(true);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions maker = new MarkerOptions()
+                        .position(latLng);
+                mMap.addMarker(maker);
+
+                CircleOptions circle = new CircleOptions()
+                        .center(latLng)
+                        .radius(100)
+                        .strokeColor(Color.BLUE)
+                        .strokeWidth(1.0f)
+                        .fillColor(Color.parseColor("#220000FF"));
+                mMap.addCircle(circle);
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    public void mCurrentLocation(View v){
+        //권한 체크
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED &&
+        ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION },
+                    REQUEST_CODE_PERMISSIONS);
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this,
+                new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null){
+                            //현재 위치
+                            LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions()
+                            .position(myLocation)
+                            .title("현재 위치"));
+
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+                            //카메라 줌
+                            mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSIONS:
+                if(ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "권한 체크 거부 됨", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+    }
+}
